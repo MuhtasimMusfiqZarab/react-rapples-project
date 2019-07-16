0cb6ea0db567d442e4ee36d34072b2d47ff4d235
muhtasimmusfiqzarab <musfiqzarab@iut-dhaka.edu>
2019-07-16 18:23:29 +0600

    layouting trading view

--- src/components/TradingView.js
+++ src/components/TradingView.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import "./TradingView.scss";
 import TradingViewWidget, { Themes } from 'react-tradingview-widget';
 
 class TradingView extends Component {
@@ -6,13 +7,40 @@ class TradingView extends Component {
     render(){
 
     return (
-        <div style={{marginTop:"5em"}}>
-          <TradingViewWidget 
-            symbol="NASDAQ:AAPL" theme={Themes.DARK} />
+      
+      <div className="grid-test">
+
+        <div className="row">
+              <div className="col-1-of-3">
+              <TradingViewWidget id="widget"
+              symbol="NASDAQ:AAPL" theme={Themes.DARK} />
+              </div>
+              <div className="col-1-of-3">
+               2nd
+              </div>
+              <div className="col-1-of-3">
+                3rd
+              </div>
         </div>
+      
+
+      </div>
+      
+      
+      
 
     );
   }
 }
 
 export default TradingView;
+
+
+
+
+                // <div className="col-1-of-3" style={{marginTop:"5em"}}>
+                // <TradingViewWidget 
+                // symbol="NASDAQ:AAPL" theme={Themes.DARK} />
+                // </div>
+
+
